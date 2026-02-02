df035d53f105ceafd1b331bda584b3352e10effe
Võ Chí Hải <vochihai7@gmail.com>
2026-02-02 07:36:49 +0700

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   tmdbApiUrl: 'https://api.themoviedb.org/3',
-  tmdbApiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzQ4YzVjZDkxYjY1YmQ1Yzc2YThkNzIxYmU2YWU5MSIsIm5iZiI6MTc2NDkyOTc2My4yNTUsInN1YiI6IjY5MzJiMGUzNDZlMjA2NzYzMTAyZDkwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1oRFy0ZJ4jZ9rROFtlwdIAz257vtoH2mUzfRL2awoCU'
+  tmdbApiKey: 'api key'
 };
